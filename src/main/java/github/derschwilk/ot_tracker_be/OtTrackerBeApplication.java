00c21bc333bb5dd38067d661e7c6700a9481472f
David Schwilk <derschwilk@macbookpro.speedport.ip>
2024-12-01 12:22:53 +0100

    Add spring
    Signed-off-by: David Schwilk <derschwilk@macbookpro.speedport.ip>

---
+++ src/main/java/github/derschwilk/ot_tracker_be/OtTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package github.derschwilk.ot_tracker_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtTrackerBeApplication.class, args);
+	}
+
+}
